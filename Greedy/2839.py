99c7a24e4a775fd274bd6ab4e6ee2ddbb20c6b3c
donghyun <donghyun.ict@gmail.com>
2021-09-19 15:29:44 +0900

    설탕 배달

--- Greedy/2839.py
+++ Greedy/2839.py
@@ -1,1 +1,16 @@
-print('test')
+N = int(input())
+
+result = 0
+
+while True:
+    if (N % 5 == 0):
+        result += N // 5
+        print(result)
+        break
+    
+    N -= 3
+    result += 1
+
+    if (N < 0):
+        print(-1)
+        break
